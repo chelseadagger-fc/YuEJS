4168818f8a426b80f048b44401995ed10149aea8
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-11 13:41:15 +0300

    added separate list at /work

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-const listItems = ["Wake up at 11am", "Teach English on Engoo", "Write Lesson Notes", "Study Javascript"]
+const listItems = ["Wake up at 11am", "Teach English on Engoo", "Write Lesson Notes", "Study Javascript"];
+const workItems = ["Log Into Engoo", "Check Classes", "Open Study Sessions"];
+
 
 app.get("/", function(req,res) {
 
     const date = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
     let day = date.toLocaleDateString("en-GB", options);
 
-    res.render("list", {day: day, listItems: listItems})
+    res.render("list", {listTitle: day, listItems: listItems});
 
+})
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work", listItems: workItems});
 })
 
 
@@ -23,13 +29,18 @@ app.post("/", function(req,res) {
 
     let newItem = req.body.addItem;
 
-    listItems.push(newItem);
 
-    res.redirect("/")
+    if (req.body.list === "Work") {
+        workItems.push(newItem);
+        res.redirect("/work");
+    } else {
+        listItems.push(newItem);
+        res.redirect("/");
+    }
 
 })
 
 
 app.listen(3000, function() {
-    console.log("Server is running; listening on port 3000")
+    console.log("Server is running; listening on port 3000");
 })
